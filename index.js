0d225f56f0b8c55e3301657e622d500405f82bb2
Coffee Brew Apps <coffeebrewapps@gmail.com>
2023-05-04 21:58:43 +0800

    Export components in index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+import TConfirmDialog from './src/components/dialog/TConfirmDialog.vue'
+import TDialog from './src/components/dialog/TDialog.vue'
+
+import TButton from './src/components/form/TButton.vue'
+import TCheckbox from './src/components/form/TCheckbox.vue'
+import TDatePicker from './src/components/form/TDatePicker.vue'
+import TInput from './src/components/form/TInput.vue'
+import TOption from './src/components/form/TOption.vue'
+import TSelect from './src/components/form/TSelect.vue'
+
+import TTable from './src/components/table/TTable.vue'
+import TProgressBar from './src/components/TProgressBar.vue'
+
+export {
+  TConfirmDialog,
+  TDialog,
+  TButton,
+  TCheckbox,
+  TDatePicker,
+  TInput,
+  TOption,
+  TSelect,
+  TTable,
+  TProgressBar
+}
