cc6a96504f5200104628f6dea3ccf3541f8d1a90
Coffee Brew Apps <coffeebrewapps@gmail.com>
2023-05-05 10:37:09 +0800

    Move demo files to demo

---
+++ demo/router/index.js
@@ -0,0 +1,87 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/DemoForm.vue')
+    },
+    {
+      path: '/form',
+      name: 'form',
+      // route level code-splitting
+      // this generates a separate chunk (Profile.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/DemoForm.vue')
+    },
+    {
+      path: '/',
+      name: 'menu1',
+      component: () => import('../views/DemoForm.vue')
+    },
+    {
+      path: '/',
+      name: 'menu2',
+      component: () => import('../views/DemoForm.vue')
+    },
+    {
+      path: '/',
+      name: 'menu3',
+      component: () => import('../views/DemoForm.vue')
+    },
+    {
+      path: '/',
+      name: 'menu4',
+      component: () => import('../views/DemoForm.vue')
+    },
+    {
+      path: '/',
+      name: 'menu5',
+      component: () => import('../views/DemoForm.vue')
+    },
+    {
+      path: '/',
+      name: 'menu6',
+      component: () => import('../views/DemoForm.vue')
+    },
+    {
+      path: '/',
+      name: 'menu7',
+      component: () => import('../views/DemoForm.vue')
+    },
+    {
+      path: '/',
+      name: 'menu8',
+      component: () => import('../views/DemoForm.vue')
+    },
+    {
+      path: '/',
+      name: 'menu9',
+      component: () => import('../views/DemoForm.vue')
+    },
+    {
+      path: '/',
+      name: 'menu10',
+      component: () => import('../views/DemoForm.vue')
+    },
+    {
+      path: '/',
+      name: 'menu11',
+      component: () => import('../views/DemoForm.vue')
+    },
+    {
+      path: '/',
+      name: 'menu12',
+      component: () => import('../views/DemoForm.vue')
+    },
+    {
+      path: '/',
+      name: 'menu13',
+      component: () => import('../views/DemoForm.vue')
+    }
+  ]
+})
+
+export default router
