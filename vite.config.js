c4fa72e3a2799f6204364ec166e9174e383881cd
Coffee Brew Apps <coffeebrewapps@gmail.com>
2023-05-05 10:35:30 +0800

    Redo build pipeline

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
@@ -8,10 +9,25 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~': fileURLToPath(new URL('./dist', import.meta.url))
     }
   },
   build: {
-    outDir: './tmp'
-  }
+    outDir: './dist',
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'CoffeebrewVueComponents',
+      fileName: 'coffeebrew_vue_components'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
+  publicDir: false
 })
